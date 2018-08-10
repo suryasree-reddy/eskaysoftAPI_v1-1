ce49003759e6696909d19a12f61d972cba03fc57
suryasree-reddy <suryasreereddyavuloori@gmail.com>
2018-08-10 11:47:00 +0530

    mvnw, mvnw.cmd removed

--- src/main/java/com/rest/eskaysoftAPI/dao/AreaDao.java
+++ src/main/java/com/rest/eskaysoftAPI/dao/AreaDao.java
@@ -1,5 +1,7 @@
 package com.rest.eskaysoftAPI.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,8 +9,9 @@ import org.springframework.stereotype.Repository;
 
 import com.rest.eskaysoftAPI.entity.Area;
 
-@Repository ("areaDao")
+@Repository("areaDao")
 @Transactional
 public interface AreaDao extends JpaRepository<Area, Long> {
+	public List<Area> findAllByOrderByAreaNameAsc();
 
 }

ec65b6776263284b1c64ba8eb6ad68504b8ad88d
suryasree-reddy <suryasreereddyavuloori@gmail.com>
2018-11-27 13:57:41 +0530

    change

--- src/main/java/com/rest/eskaysoftAPI/controller/UserController.java
+++ src/main/java/com/rest/eskaysoftAPI/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,7 +29,6 @@ import com.rest.eskaysoftAPI.exception.AppException;
 import com.rest.eskaysoftAPI.exception.NotFoundException;
 import com.rest.eskaysoftAPI.exception.ResourceNotFoundException;
 import com.rest.eskaysoftAPI.model.ApiResponse;
-import com.rest.eskaysoftAPI.model.LoginRequest;
 import com.rest.eskaysoftAPI.model.UserIdentityAvailability;
 import com.rest.eskaysoftAPI.model.UserInformation;
 import com.rest.eskaysoftAPI.repository.DistrictsRepository;
@@ -106,9 +104,8 @@ public class UserController {
 		});
 		return users;
 	}
-	
 
-	@PutMapping("/updateUser")
+	@PostMapping("/updateUser")
 	public ResponseEntity<?> updateUser(@Valid @RequestBody UserInformation userproRequest) {
 
 		Districts dis = disrepo.findById(userproRequest.getDistrictId()).orElseThrow(
